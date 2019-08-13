78473d3989bac385f807ee4d6b5ca1e702e132f9
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-13 19:36:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
 def reverse_each_word(sentence)
+
+  #using each
   sentence_array = sentence.split
-  reverse_sentence_array = []
-  sentence_array.each() do |word|
-  reverse_sentence_array.push(word.reverse)
-end
-  return reverse_sentence_array.join(" ")
+  #reverse_sentence_array = []
+  #sentence_array.each() do |word|
+  #reverse_sentence_array.push(word.reverse)
+#end
+
+
+  #using collect
+  reverse_sentence_array = sentence_array.collect |word| do
+    word.reverse
+  end
+    return reverse_sentence_array.join(" ")
 end
